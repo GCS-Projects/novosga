8cf878a5f2f59546e1b9945a988174354f2e772e
ElmarRoberto <elmarunb@gmail.com>
2016-11-26 11:09:33 -0200

    Changing host

--- src/Novosga/Test/TestConectionDB_mysql.php
+++ src/Novosga/Test/TestConectionDB_mysql.php
@@ -2,8 +2,15 @@
 	class TestConectionDB_mysql extends PHPUnit_Framework_TestCase {
 		
 		public function testConectionWithDB() {
+			
+			DEFINE('DB_SERVER', '127.0.0.1');
+			DEFINE('DB_USERNAME', 'root');
+			DEFINE('DB_PASSWORD', 'phprs');
+			DEFINE('DB_DATABASE', '');
+			
 			$conection = NULL;
-			$conection = mysqli_connect("172.17.0.2", "root", "phprs");
+			$conection = mysqli_connect("DB_SERVER", "DB_USERNAME", "DB_PASSWORD", "");
+
 			if (!$conection) {
 				$this->assertTrue(false);
 			} else {
