545c11fd2b04ce80d298c14ba17e129cb559c9f1
ElmarRoberto <elmarunb@gmail.com>
2016-11-26 11:29:48 -0200

    DatabaseName

--- src/Novosga/Test/TestConectionDB_mysql.php
+++ src/Novosga/Test/TestConectionDB_mysql.php
@@ -3,13 +3,13 @@
 		
 		public function testConectionWithDB() {
 			
-			DEFINE('DB_SERVER', '127.0.0.1');
+			DEFINE('DB_SERVER', '172.17.0.2');
 			DEFINE('DB_USERNAME', 'root');
 			DEFINE('DB_PASSWORD', 'phprs');
-			DEFINE('DB_DATABASE', '');
+			DEFINE('DB_DATABASE', 'phprs');
 			
 			$conection = NULL;
-			$conection = mysqli_connect("DB_SERVER", "DB_USERNAME", "DB_PASSWORD", "");
+			$conection = mysqli_connect("DB_SERVER", "DB_USERNAME", "DB_PASSWORD", "DB_DATABASE");
 
 			if (!$conection) {
 				$this->assertTrue(false);
